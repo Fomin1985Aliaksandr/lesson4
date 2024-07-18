635105b75227ff246d0de1e2e5364b6110e8e8b2
aliaksandr <96591290+Fomin1985Aliaksandr@users.noreply.github.com>
2024-07-18 11:08:28 +0300

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,71 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Scanner;
+public class App {
 
 
 
 
-{
+    @Test//Test1//
+    public void raduisIsNull(){
+
+        double raduis = 0;
+        Assert.assertTrue(raduis==0, "немо");
+        System.out.println(" неможет быть равен Null ");
+    }
 
 
-    public static void main( String[] args ){
+    @Test//test2
+
+    public void negativRadius(){
+        double raduis = -3;
+        Assert.assertFalse(raduis<0, "Не может быть отрицательным ");
+
+    }
+
+    @Test//Test3
+    public void positivRadius(){
+        double radius = 5;
+        Assert.assertEquals(radius>0, "positiv");
+    }
+
+    public static void main (String[] args){
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Для определения длинны окружности задайте радиус ");
+        double radius;
+
+
+
+
+
+
+        radius = scanner.nextInt();
+        double result = 2*Math.PI*radius;
+
+        if(radius==0){
+            System.out.println("Радиус не может быть равен NULL");
+        }
+        else if(radius<0){
+            System.out.println(" Радиус не может быть отрицацельный ");
+        }
+        else{
+            System.out.println("Радус равен " + result );
+        }
+
+
+
+
 
 
     }
+
+
+
 }
